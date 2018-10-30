873777d92124d0c52b88a49a986e5a80f657a678
Kevin Wu <kevincwu0@gmail.com>
2018-10-30 16:16:00 -0400

    Containerize Flask app with Docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,16 @@
 # services/users/project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig') # new
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+import sys
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
