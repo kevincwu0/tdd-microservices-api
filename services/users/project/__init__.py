f165d6eeeb79e44ffeef90c8ea805b40afafbd7d
Kevin Wu <kevincwu0@gmail.com>
2018-10-31 18:28:38 -0400

    Add RESTful routes and tests

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,20 +1,29 @@
 # services/users/project/__init__.py
 
 import os
-from flask import Flask, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy  # new
 
-# instantiate the app
-app = Flask(__name__)
+# instantiate the db
+db = SQLAlchemy()
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
 
-import sys
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+    return app
